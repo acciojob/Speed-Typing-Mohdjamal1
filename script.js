d81d00b41b558e85cf26af2c5c523a613964e734
Mohd Jamal <79620177+Mohdjamal1@users.noreply.github.com>
2023-06-24 12:38:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+ document.getElementById('quoteInput').value.addEventListener('keyDown',getQuote);
+
+function getQuote() {
+       
+        fetch("http://api.quotable.io/random")
+          .then(response => response.json())
+          .then(data => {
+            let div = document.querySelector('.quote-display');
+            div.innerText = 'data';
+          })
+          .catch(error => console.error(error));
+      }
